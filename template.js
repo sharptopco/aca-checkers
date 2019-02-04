9f2078e8dcc5e22d8317b336de3be04ce057f1a7
Tony Erskine <tony@sharptop.co>
2019-02-04 14:27:02 -0500

    refactoring template.js

--- template.js
+++ template.js
@@ -1,35 +1,40 @@
-function cellTemplate(rowNum, cellNum) {
-    var isCellEven = true
-    var isRowEven = true
-    if (cellNum % 2 == 1) isCellEven = false
-    if (rowNum % 2 == 1) isRowEven = false
+function checkerTemplate(rowNum) {
+    if (rowNum <= 3) {
+        return `<div class="checker red-checker"></div>`
+    } else if (rowNum >= 6) {
+        return `<div class="checker black-checker"></div>`
+    } else {
+        return `<div class="checker black-checker" hidden="true"></div>`
+    }
+}
 
-    if (isCellEven != isRowEven) {
+function parity(num) {
+    return (num % 2 == 0) ? 'even' : 'odd'
+}
+
+function cellColor(cellNum, rowNum) {
+    return parity(cellNum) == parity(rowNum) ? 'red' : 'black'
+}
+
+function cellTemplate(rowNum, cellNum) {
+    if (cellColor(cellNum, rowNum) === 'black') {
         // Black Cell
-        let cellString = `<div id="cell-${rowNum}-${cellNum}" class="cell black">`
-        if (rowNum <= 3) {
-            cellString = cellString + `<div class="checker red-checker"></div>`
-        } else if (rowNum >= 6) {
-            cellString = cellString + `<div class="checker black-checker"></div>`
-        } else {
-            cellString = cellString + `<div class="checker black-checker" hidden="true"></div>`
-        }
-        cellString = cellString + `</div>`
-        return cellString
+        return `
+            <div id="cell-${rowNum}-${cellNum}" class="cell black">
+                ${checkerTemplate(rowNum)}
+            </div>
+        `
     } else
         // White Cell
         return `<div id="cell-${rowNum}-${cellNum}" class="cell red"></div>`
 }
 
 function rowTemplate(rowNum) {
-    console.log(`drawing row number ${rowNum}`)
-    var rowString = ''
-    rowString = rowString + `<div id="row-${rowNum}" class="row">`
+    var rowString = `<div id="row-${rowNum}" class="row">`
     for (var cellNum=1; cellNum<=8; cellNum++) {
         rowString = rowString + cellTemplate(rowNum, cellNum)
     }
-    rowString = rowString + `</div>`
-    return rowString
+    return rowString + `</div>`
 }
 
 function boardTemplate() {
