b269784a12e8e8508dc652736eae769783258e0d
Tony Erskine <tony@sharptop.co>
2019-02-07 15:25:03 -0500

    separated board rendering and checker rendering into separate files

--- checkers.js
+++ checkers.js
@@ -68,38 +68,6 @@ var checkers = [
     whiteChecker(),
 ]
 
-function renderBoard() {
-    return `
-        ${renderRow(1)}
-        ${renderRow(2)}
-        ${renderRow(3)}
-        ${renderRow(4)}
-        ${renderRow(5)}
-        ${renderRow(6)}
-        ${renderRow(7)}
-        ${renderRow(8)}
-    `
-}
-
-function renderRow(rowNum) {
-    return `
-        <div id="row-${rowNum}" class="row">
-            ${renderCell(rowNum, 1)}
-            ${renderCell(rowNum, 2)}
-            ${renderCell(rowNum, 3)}
-            ${renderCell(rowNum, 4)}
-            ${renderCell(rowNum, 5)}
-            ${renderCell(rowNum, 6)}
-            ${renderCell(rowNum, 7)}
-            ${renderCell(rowNum, 8)}
-        </div>
-    `
-}
-
-function renderCell(rowNum, cellNum) {
-    return `<div id="cell-${rowNum}-${cellNum}" class="cell ${cellColor(cellNum, rowNum)}"></div>`
-}
-
 function renderCheckers() {
     for (let i = 0; i<checkers.length; i++){
         let checker = checkers[i]
@@ -119,13 +87,3 @@ function renderChecker(rowNum) {
         return `<div class="checker black-checker" hidden="true"></div>`
     }
 }
-
-/*** Helper Methods ***/
-function parity(num) {
-    return (num % 2 == 0) ? 'even' : 'odd'
-}
-
-function cellColor(cellNum, rowNum) {
-    return parity(cellNum) == parity(rowNum) ? 'white' : 'black'
-}
-
