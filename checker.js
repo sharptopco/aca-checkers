30921b2beeb22a4b4127f09b85e6d6a7bb78a974
Tony Erskine <tony@sharptop.co>
2019-03-21 14:01:45 -0400

    - renamed template.js to checkers.js - changed the click handler to checkers

--- checker.js
+++ checker.js
@@ -28,42 +28,6 @@ var checkers = [
 
 ]
 
-
-function renderBoard() {
-    return `
-        ${renderRow(1)}
-        ${renderRow(2)}
-        ${renderRow(3)}
-        ${renderRow(4)}
-        ${renderRow(5)}
-        ${renderRow(6)}
-        ${renderRow(7)}
-        ${renderRow(8)}
-    `
-}
-
-function renderRow(rowNum) {
-    return `
-        <div id="row-${rowNum}" class="row">
-            ${renderCell(rowNum, 1)}
-            ${renderCell(rowNum, 2)}
-            ${renderCell(rowNum, 3)}
-            ${renderCell(rowNum, 4)}
-            ${renderCell(rowNum, 5)}
-            ${renderCell(rowNum, 6)}
-            ${renderCell(rowNum, 7)}
-            ${renderCell(rowNum, 8)}
-        </div>
-    `
-}
-
-function renderCell(rowNum, cellNum) {
-    if (cellColor(cellNum, rowNum) === 'black') {
-        return `<div id="cell-${rowNum}-${cellNum}" class="cell black"></div>`
-    } else
-        return `<div id="cell-${rowNum}-${cellNum}" class="cell white"></div>`
-}
-
 function renderCheckers(){
     console.log('rendering checkers')
     for(let i=0; i<checkers.length; i++) {
@@ -75,15 +39,6 @@ function renderCheckers(){
 
 function renderChecker(color) {
     return `<div class="checker ${color}-checker"></div>`
-}
-
-/*** Helper Methods ***/
-function parity(num) {
-    return (num % 2 == 0) ? 'even' : 'odd'
-}
-
-function cellColor(cellNum, rowNum) {
-    return parity(cellNum) == parity(rowNum) ? 'white' : 'black'
 }
 
 function selectChecker() {
